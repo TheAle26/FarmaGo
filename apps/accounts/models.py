aec3df4e5049be1551818cf9c0acb6dcbdf907c1
Alejo <alejo.vincent26@gmail.com>
2025-10-20 15:03:50 -0300

    base de incio

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    username = None  # quitamos username
+    #username = None  # quitamos username
     email = models.EmailField(unique=True)  # login con email
     activo = models.BooleanField(default=True)
 
